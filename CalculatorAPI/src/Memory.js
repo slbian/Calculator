1e72ba11964f69d42a16ad2a179d9036b8a9c656
Sharon Bian <sharon.bian@liveramp.com>
2019-07-03 18:29:06 -0700

    add memory

---
+++ CalculatorAPI/src/Memory.js
@@ -0,0 +1,5 @@
+export default class Memory {
+  constructor() {
+    this.memory = { sharonb: 1, helenb: 2 };
+  }
+}
--- CalculatorAPI/src/index.js
+++ CalculatorAPI/src/index.js
@@ -1,16 +1,18 @@
 import express from '../node_modules/express';
 import cors from '../node_modules/cors/lib';
 import '../node_modules/dotenv/config';
+import Memory from './Memory';
 
 const PORT = process.env.PORT;
 const app = express();
 
+const memory = new Memory();
 // middleware
 app.use(cors());
 
 // routes
-app.get('/', (req, res) => {
-  res.send('suppupfs!');
+app.get('/all-users', (req, res) => {
+  res.json(memory.cache);
 });
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}!`));
