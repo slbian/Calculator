3bf0c4b6723155f055b4b085e110455bb012753d
slbian <sharon.bian@liveramp.com>
2020-08-13 19:30:22 -0400

    seed massive executions

--- calculatorDB/seeds/02_seed_users.js
+++ calculatorDB/seeds/02_seed_users.js
@@ -1,33 +1,54 @@
 const now = new Date().toISOString();
-exports.seed = function(knex) {
-  return knex('users')
+
+const users = [
+  {
+    username: "sbian",
+    created_at: now,
+    updated_at: now,
+    themeId: 1,
+    hashedPassword:
+      "$argon2i$v=19$m=4096,t=3,p=1$FjfnJA8D++gR6u+MM4Cd0Q$zHfv7F6dFAfNZdrTv0fvbVJ/IPJQI/s8j5m9vt5WGGM",
+  },
+  {
+    username: "chuck",
+    created_at: now,
+    updated_at: now,
+    themeId: 2,
+    hashedPassword:
+      "$argon2i$v=19$m=4096,t=3,p=1$FjfnJA8D++gR6u+MM4Cd0Q$zHfv7F6dFAfNZdrTv0fvbVJ/IPJQI/s8j5m9vt5WGGM",
+  },
+  {
+    username: "ellen",
+    created_at: now,
+    updated_at: now,
+    themeId: 3,
+    hashedPassword:
+      "$argon2i$v=19$m=4096,t=3,p=1$FjfnJA8D++gR6u+MM4Cd0Q$zHfv7F6dFAfNZdrTv0fvbVJ/IPJQI/s8j5m9vt5WGGM",
+  },
+  {
+    username: "dylan",
+    created_at: now,
+    updated_at: now,
+    themeId: 3,
+    hashedPassword:
+      "$argon2i$v=19$m=4096,t=3,p=1$FjfnJA8D++gR6u+MM4Cd0Q$zHfv7F6dFAfNZdrTv0fvbVJ/IPJQI/s8j5m9vt5WGGM",
+  },
+  {
+    username: "eloise",
+    created_at: now,
+    updated_at: now,
+    themeId: 3,
+    hashedPassword:
+      "$argon2i$v=19$m=4096,t=3,p=1$FjfnJA8D++gR6u+MM4Cd0Q$zHfv7F6dFAfNZdrTv0fvbVJ/IPJQI/s8j5m9vt5WGGM",
+  },
+];
+
+exports.seed = function (knex) {
+  return knex("users")
     .del()
-    .then(function() {
-      return knex('users').insert([
-        {
-          username: 'sbian',
-          created_at: now,
-          updated_at: now,
-          themeId: 1,
-          hashedPassword:
-            '$argon2i$v=19$m=4096,t=3,p=1$FjfnJA8D++gR6u+MM4Cd0Q$zHfv7F6dFAfNZdrTv0fvbVJ/IPJQI/s8j5m9vt5WGGM',
-        },
-        {
-          username: 'chuck',
-          created_at: now,
-          updated_at: now,
-          themeId: 2,
-          hashedPassword:
-            '$argon2i$v=19$m=4096,t=3,p=1$FjfnJA8D++gR6u+MM4Cd0Q$zHfv7F6dFAfNZdrTv0fvbVJ/IPJQI/s8j5m9vt5WGGM',
-        },
-        {
-          username: 'ellen',
-          created_at: now,
-          updated_at: now,
-          themeId: 3,
-          hashedPassword:
-            '$argon2i$v=19$m=4096,t=3,p=1$FjfnJA8D++gR6u+MM4Cd0Q$zHfv7F6dFAfNZdrTv0fvbVJ/IPJQI/s8j5m9vt5WGGM',
-        },
-      ]);
+    .then(function () {
+      return knex("users").insert(users);
     });
 };
+
+exports.users = users;
--- calculatorDB/seeds/03_seed_executions.js
+++ calculatorDB/seeds/03_seed_executions.js
@@ -1,30 +1,76 @@
+const { users } = require("./02_seed_users");
+
 const now = new Date().toISOString();
-exports.seed = function(knex) {
-  return knex('executions')
-    .del()
-    .then(function() {
-      return knex('executions').insert([
-        {
-          userId: '1',
-          equation: '3+4+5+6',
-          score: 2,
-          created_at: now,
-          updated_at: now,
-        },
-        {
-          userId: '1',
-          equation: '1231273918*7',
-          score: 30,
-          created_at: now,
-          updated_at: now,
-        },
-        {
-          userId: '2',
-          equation: '3+4',
-          score: 1,
-          created_at: now,
-          updated_at: now,
-        },
-      ]);
-    });
+const TOTAL_PER = 10000;
+exports.seed = async function (knex) {
+  await knex("executions").del();
+
+  const x = getLotsOfExecutions();
+
+  await knex("executions").insert(x[0]);
+  console.log("...");
+  await knex("executions").insert(x[1]);
+  console.log("...");
+  await knex("executions").insert(x[2]);
+  console.log("...");
+  await knex("executions").insert(x[3]);
+  console.log("...");
+  await knex("executions").insert(x[4]);
+  console.log("...");
+  await knex("executions").insert(x[0]);
+  console.log("...");
+  await knex("executions").insert(x[1]);
+  console.log("...");
+  await knex("executions").insert(x[2]);
+  console.log("...");
+  await knex("executions").insert(x[3]);
+  console.log("...");
+  await knex("executions").insert(x[4]);
+  console.log("...");
+  await knex("executions").insert(x[0]);
+  console.log("...");
+  await knex("executions").insert(x[1]);
+  console.log("...");
+  await knex("executions").insert(x[2]);
+  console.log("...");
+  await knex("executions").insert(x[3]);
+  console.log("...");
+  await knex("executions").insert(x[4]);
+  console.log("...");
+  await knex("executions").insert(x[0]);
+  console.log("...");
+  await knex("executions").insert(x[1]);
+  console.log("...");
+  await knex("executions").insert(x[2]);
+  console.log("...");
+  await knex("executions").insert(x[3]);
+  console.log("...");
+  await knex("executions").insert(x[4]);
+  console.log("...");
+
+  console.log("SUCCESS");
 };
+
+function getLotsOfExecutions() {
+  const equations = ["3+4+5+6", "3-4-5-6", "12+2+5-4", "13+3+5-1", "53-4+5+6"];
+
+  const all = [];
+
+  users.forEach((user, j) => {
+    const forUser = [];
+    for (let i = 0; i < TOTAL_PER; i++) {
+      const equation = equations[Math.floor(Math.random() * equations.length)];
+      const score = Number(eval(equation).toString().length);
+      forUser.push({
+        userId: j + 1,
+        equation,
+        score,
+        created_at: now,
+        updated_at: now,
+      });
+    }
+    all.push(forUser);
+  });
+
+  return all;
+}
